79241071418056bdca17f05db091680f5472abae
Shresthap21 <pandeyshrestha2111@gmail.com>
2025-06-22 01:09:15 +0530

    chore(data): update services data

---
+++ data/services.js
@@ -0,0 +1,238 @@
+export const serviceData = {
+  "market-research": {
+    "title": "Market Research",
+    "tagline": "Gain insight into how our research-driven approach informs every design.",
+    "section": {
+      "heading": "Where Insight Shapes Design",
+      "description": "At Fabrizones, our Market Research team dives deep into client needs, lifestyle patterns, and evolving trends to shape meaningful design strategies. We don't just follow the market—we listen, learn, and interpret it for your space. By combining practical insights with creative direction, we ensure each project reflects your personality while meeting real-world needs.",
+      "image": "/assets/images/our-services/card4.jpg",
+      "buttons": [
+        { "label": "Schedule a Consultation", "link": "#" },
+        { "label": "Explore Services", "link": "#" }
+      ]
+    },
+    "otherServices": [
+      {
+        "title": "Project Estimation",
+        "description": "We provide accurate estimates and proposals that balance quality, materials, and budget perfectly.",
+        "icon": "/assets/images/our-services/icon2.png",
+        "image": "/assets/images/our-services/card2.jpg"
+      },
+      {
+        "title": "Inhouse Fabrication",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon6.png",
+        "image": "/assets/images/our-services/card6.jpg"
+      },
+      {
+        "title": "Warehouse Facility",
+        "description": "We manage, store, and dispatch materials with efficiency to support seamless project execution.",
+        "icon": "/assets/images/our-services/icon1.png",
+        "image": "/assets/images/our-services/card1.jpg"
+      },
+      {
+        "title": "Market Research",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon4.png",
+        "image": "/assets/images/our-services/card4.jpg"
+      }
+    ]
+  },
+  "inhouse-fabrication": {
+    "title": "Inhouse Fabrication",
+    "tagline": "Precision-Made. Quality-Driven. Always In Style.",
+    "section": {
+      "heading": "Where Craft Meets Technology",
+      "description": "Our Inhouse Fabrication facility is the backbone of Fabroneer’s commitment to excellence. Armed with advanced machinery and guided by seasoned craftsmen, we create high-quality soft furnishings that meet the highest design standards. From automated production runs to delicate hand-finished details, our fabrication process ensures every stitch, fold, and edge meets perfection.",
+      "image": "/assets/images/our-services/card6.jpg",
+      "buttons": [
+        { "label": "Schedule a Consultation", "link": "#" },
+        { "label": "Explore Services", "link": "#" }
+      ]
+    },
+    "otherServices": [
+      {
+        "title": "Project Estimation",
+        "description": "We provide accurate estimates and proposals that balance quality, materials, and budget perfectly.",
+        "icon": "/assets/images/our-services/icon2.png",
+        "image": "/assets/images/our-services/card2.jpg"
+      },
+      {
+        "title": "Inhouse Fabrication",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon6.png",
+        "image": "/assets/images/our-services/card6.jpg"
+      },
+      {
+        "title": "Warehouse Facility",
+        "description": "We manage, store, and dispatch materials with efficiency to support seamless project execution.",
+        "icon": "/assets/images/our-services/icon1.png",
+        "image": "/assets/images/our-services/card1.jpg"
+      },
+      {
+        "title": "Market Research",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon4.png",
+        "image": "/assets/images/our-services/card4.jpg"
+      }
+    ]
+  },
+
+  "design-development": {
+    "title": "Inhouse Fabrication",
+    "tagline": "Precision-Made. Quality-Driven. Always In Style.",
+    "section": {
+      "heading": "Where Craft Meets Technology",
+      "description": "Our Inhouse Fabrication facility is the backbone of Fabroneer’s commitment to excellence. Armed with advanced machinery and guided by seasoned craftsmen, we create high-quality soft furnishings that meet the highest design standards. From automated production runs to delicate hand-finished details, our fabrication process ensures every stitch, fold, and edge meets perfection.",
+      "image": "/assets/images/our-services/card6.jpg",
+      "buttons": [
+        { "label": "Schedule a Consultation", "link": "#" },
+        { "label": "Explore Services", "link": "#" }
+      ]
+    },
+    "otherServices": [
+      {
+        "title": "Project Estimation",
+        "description": "We provide accurate estimates and proposals that balance quality, materials, and budget perfectly.",
+        "icon": "/assets/images/our-services/icon2.png",
+        "image": "/assets/images/our-services/card2.jpg"
+      },
+      {
+        "title": "Inhouse Fabrication",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon6.png",
+        "image": "/assets/images/our-services/card6.jpg"
+      },
+      {
+        "title": "Warehouse Facility",
+        "description": "We manage, store, and dispatch materials with efficiency to support seamless project execution.",
+        "icon": "/assets/images/our-services/icon1.png",
+        "image": "/assets/images/our-services/card1.jpg"
+      },
+      {
+        "title": "Market Research",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon4.png",
+        "image": "/assets/images/our-services/card4.jpg"
+      }
+    ]
+  },
+
+    "warehouse-facility": {
+    "title": "Inhouse Fabrication",
+    "tagline": "Precision-Made. Quality-Driven. Always In Style.",
+    "section": {
+      "heading": "Where Craft Meets Technology",
+      "description": "Our Inhouse Fabrication facility is the backbone of Fabroneer’s commitment to excellence. Armed with advanced machinery and guided by seasoned craftsmen, we create high-quality soft furnishings that meet the highest design standards. From automated production runs to delicate hand-finished details, our fabrication process ensures every stitch, fold, and edge meets perfection.",
+      "image": "/assets/images/our-services/card6.jpg",
+      "buttons": [
+        { "label": "Schedule a Consultation", "link": "#" },
+        { "label": "Explore Services", "link": "#" }
+      ]
+    },
+    "otherServices": [
+      {
+        "title": "Project Estimation",
+        "description": "We provide accurate estimates and proposals that balance quality, materials, and budget perfectly.",
+        "icon": "/assets/images/our-services/icon2.png",
+        "image": "/assets/images/our-services/card2.jpg"
+      },
+      {
+        "title": "Inhouse Fabrication",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon6.png",
+        "image": "/assets/images/our-services/card6.jpg"
+      },
+      {
+        "title": "Warehouse Facility",
+        "description": "We manage, store, and dispatch materials with efficiency to support seamless project execution.",
+        "icon": "/assets/images/our-services/icon1.png",
+        "image": "/assets/images/our-services/card1.jpg"
+      },
+      {
+        "title": "Market Research",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon4.png",
+        "image": "/assets/images/our-services/card4.jpg"
+      }
+    ]
+  },
+    "project-estimation": {
+    "title": "Inhouse Fabrication",
+    "tagline": "Precision-Made. Quality-Driven. Always In Style.",
+    "section": {
+      "heading": "Where Craft Meets Technology",
+      "description": "Our Inhouse Fabrication facility is the backbone of Fabroneer’s commitment to excellence. Armed with advanced machinery and guided by seasoned craftsmen, we create high-quality soft furnishings that meet the highest design standards. From automated production runs to delicate hand-finished details, our fabrication process ensures every stitch, fold, and edge meets perfection.",
+      "image": "/assets/images/our-services/card6.jpg",
+      "buttons": [
+        { "label": "Schedule a Consultation", "link": "#" },
+        { "label": "Explore Services", "link": "#" }
+      ]
+    },
+    "otherServices": [
+      {
+        "title": "Project Estimation",
+        "description": "We provide accurate estimates and proposals that balance quality, materials, and budget perfectly.",
+        "icon": "/assets/images/our-services/icon2.png",
+        "image": "/assets/images/our-services/card2.jpg"
+      },
+      {
+        "title": "Inhouse Fabrication",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon6.png",
+        "image": "/assets/images/our-services/card6.jpg"
+      },
+      {
+        "title": "Warehouse Facility",
+        "description": "We manage, store, and dispatch materials with efficiency to support seamless project execution.",
+        "icon": "/assets/images/our-services/icon1.png",
+        "image": "/assets/images/our-services/card1.jpg"
+      },
+      {
+        "title": "Market Research",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon4.png",
+        "image": "/assets/images/our-services/card4.jpg"
+      }
+    ]
+  },
+    "value-engineering": {
+    "title": "Inhouse Fabrication",
+    "tagline": "Precision-Made. Quality-Driven. Always In Style.",
+    "section": {
+      "heading": "Where Craft Meets Technology",
+      "description": "Our Inhouse Fabrication facility is the backbone of Fabroneer’s commitment to excellence. Armed with advanced machinery and guided by seasoned craftsmen, we create high-quality soft furnishings that meet the highest design standards. From automated production runs to delicate hand-finished details, our fabrication process ensures every stitch, fold, and edge meets perfection.",
+      "image": "/assets/images/our-services/card6.jpg",
+      "buttons": [
+        { "label": "Schedule a Consultation", "link": "#" },
+        { "label": "Explore Services", "link": "#" }
+      ]
+    },
+    "otherServices": [
+      {
+        "title": "Project Estimation",
+        "description": "We provide accurate estimates and proposals that balance quality, materials, and budget perfectly.",
+        "icon": "/assets/images/our-services/icon2.png",
+        "image": "/assets/images/our-services/card2.jpg"
+      },
+      {
+        "title": "Inhouse Fabrication",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon6.png",
+        "image": "/assets/images/our-services/card6.jpg"
+      },
+      {
+        "title": "Warehouse Facility",
+        "description": "We manage, store, and dispatch materials with efficiency to support seamless project execution.",
+        "icon": "/assets/images/our-services/icon1.png",
+        "image": "/assets/images/our-services/card1.jpg"
+      },
+      {
+        "title": "Market Research",
+        "description": "We study market trends to guide practical and elegant design decisions for every unique project.",
+        "icon": "/assets/images/our-services/icon4.png",
+        "image": "/assets/images/our-services/card4.jpg"
+      }
+    ]
+  }
+}
