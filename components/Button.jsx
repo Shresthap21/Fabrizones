5b108eefc5c7ff5b5435038df17858fcdeb5acde
Shresthap21 <pandeyshrestha2111@gmail.com>
2025-06-24 10:03:18 +0530

    feat: button ui fix

--- components/Button.jsx
+++ components/Button.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { LuArrowRight } from "react-icons/lu";
 
 const Button = ({ isHomePage, size = "default", isNavbar = false }) => {
+  const sizeClasses = size === "small" ? 'text-sm p-2 gap-2' : 'text-lg p-3 gap-3';
   const baseSize = "text-lg p-3 gap-3";
   const hoverSize = isNavbar ? "group-hover:text-xs group-hover:p-1 group-hover:gap-2" : "";
 
@@ -22,7 +23,7 @@ const Button = ({ isHomePage, size = "default", isNavbar = false }) => {
     <button
       className={`font-urbanist font-normal bg-[#111B29] rounded-md flex items-center justify-center text-white 
         transform transition-all duration-300 ease-in-out group 
-        ${scaleClass} ${baseSize} ${hoverSize}`}
+        ${scaleClass} ${baseSize} ${hoverSize} ${sizeClasses}`}
     >
       <span
         className={`rounded-full p-1 transition-all duration-300 ease-in-out transform 
