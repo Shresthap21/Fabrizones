d81e3d0b4c2d12fabaf99c1a6acf1f704017655e
Shresthap21 <pandeyshrestha2111@gmail.com>
2025-06-25 10:23:55 +0530

    feat: update metadata

--- app/layout.js
+++ app/layout.js
@@ -7,24 +7,32 @@ import Contact from "@/components/Contact";
 
 export const metadata = {
   title: "Fabrizones",
-  description: "Experience the best in custom drapes, upholstery, and home automation—where every detail enhances your home with elegance and every design creates a seamless blend of comfort and style.",
+  description:
+    "Experience the best in custom drapes, upholstery, and home automation—where every detail enhances your home with elegance and every design creates a seamless blend of comfort and style.",
   openGraph: {
+    title: "Fabrizones",
+    description:
+      "Experience the best in custom drapes, upholstery, and home automation—where every detail enhances your home with elegance and every design creates a seamless blend of comfort and style.",
+    url: "https://fabrizones.vercel.app",
+    siteName: "Fabrizones",
     images: [
       {
-        url: "/assets/images/graph-img.png", // Update the path to your image
+        url: "https://fabrizones.vercel.app/assets/images/graph-img.png",
         width: 1200,
         height: 630,
         alt: "Fabrizones - Custom Drapes, Upholstery, and Home Automation",
       },
     ],
+    locale: "en_US",
+    type: "website",
   },
 };
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        // className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         {children}
